e1f1809e9d72acacf4c229306616114cd6d1ab70
Gustavo Hinkel <78951385+Ophaneom@users.noreply.github.com>
2021-03-26 22:54:59 -0300

    Updated config.json file.

--- MiniBars/Config.cs
+++ MiniBars/Config.cs
@@ -8,7 +8,7 @@ namespace MiniBars
 {
     public class Config
     {
-        public int Bars_Theme { get; set; } = 1;
+        public int Bars_Theme { get; set; } = 2;
         public bool Only_One_Theme { get; set; } = false;
         public bool Show_Monsters_HP { get; set; } = false;
         public bool Show_Full_Life { get; set; } = true;
