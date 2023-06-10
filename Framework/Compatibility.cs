7d95278fd6a68d0e8393360524d09b4a8a42950f
Gustavo Hinkel <78951385+Ophaneom@users.noreply.github.com>
2023-06-10 04:15:59 -0300

    Updated "Swim Mod" compatibility.

--- Framework/Compatibility.cs
+++ Framework/Compatibility.cs
@@ -6,18 +6,18 @@ namespace MiniBars.Framework
     {
         public static bool IsBlackListed(string _monster_name)
         {
-            if (Game1.player.currentLocation.Name == "UnderwaterBeach")
+            if (ModEntry.instance.Helper.ModRegistry.IsLoaded("aedenthorn.Swim"))
             {
-                if (_monster_name == "Serpent") return true;
-                else if (_monster_name == "Rock Crab") return true;
-                else return false;
+                if (Game1.player.currentLocation.Name == "Custom_UnderwaterBeach" ||
+                    Game1.player.currentLocation.Name == "Custom_UnderwaterMountain" ||
+                    Game1.player.currentLocation.Name == "Custom_UnderwaterTown" ||
+                    Game1.player.currentLocation.Name == "Custom_ScubaAbigailCave")
+                {
+                    return true;
+                }
             }
-            else if (Game1.player.currentLocation.Name == "UnderwaterMountain")
-            {
-                if (_monster_name == "Pepper Rex") return true;
-                else return false;
-            }
-            else if (Game1.player.currentLocation.Name == "Slime Hutch")
+            
+            if (Game1.player.currentLocation.Name == "Slime Hutch")
             {
                 return true;
             }
--- ModEntry.cs
+++ ModEntry.cs
@@ -1,6 +1,9 @@
 ﻿using StardewModdingAPI;
 using MiniBars.Framework.Rendering;
 using MiniBars.Framework;
+using StardewModdingAPI.Events;
+using Microsoft.Xna.Framework;
+using StardewValley;
 
 namespace MiniBars
 {
@@ -17,9 +20,18 @@ namespace MiniBars
             Textures.LoadTextures();
 
             helper.Events.Display.RenderedWorld += Renderer.OnRendered;
+            helper.Events.Input.ButtonPressed += OnButtonPressed;
             helper.ConsoleCommands.Add("minibars_theme", "Change the bars theme.\nUsage: minibars_theme '1 or 2'", Commands.Theme);
             helper.ConsoleCommands.Add("minibars_showfull", "Enable or disable showing healthbars when monsters full health.\nUsage: minibars_showfull 'true or false'", Commands.ShowFullLife);
             helper.ConsoleCommands.Add("minibars_range", "Enable or disable the range verification.\nUsage: minibars_range 'true or false'", Commands.RangeVerification);
+        }
+
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            if (e.Button == SButton.T)
+            {
+                this.Monitor.Log($"Location: {Game1.currentLocation.Name}", LogLevel.Warn);
+            }
         }
     }
 }
